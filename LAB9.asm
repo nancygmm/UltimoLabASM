4e7cc8aa96dac34393d81c21a0f13ba4c06c8ea9
Nancy Mazariegos <99931623+nancygmm@users.noreply.github.com>
2023-05-24 22:12:16 -0600

    Update LAB9.asm
    Importaciones

--- LAB9.asm
+++ LAB9.asm
@@ -1,1 +1,19 @@
+;Universidad del Valle de Guatemala
+;Nancy Gabriela Mazariegos Molina (22513)
+;Jose Santiago Pereira Alvarado (22318)
+;Curso: Organización de computadoras y Assembler
+;Profesor: Roger Diaz
+;Descripcion: Laboratorio 9
+;24/05/2023
 
+includelib libcmt.lib
+includelib libvcruntime.lib
+includelib libucrt.lib
+includelib legacy_stdio_definitions.lib
+
+.386
+.model flat, stdcall, C
+.stack 4096
+
+
+.data
